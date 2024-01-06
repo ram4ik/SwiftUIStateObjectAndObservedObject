b99f7de0a10cb5c368bac6a363885f2f72cdaf8b
Ramill <ram4ik@hotmail.com>
2024-01-06 08:23:25 +0200

    Initial Commit

---
+++ SwiftUIStateObjectAndObservedObject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIStateObjectAndObservedObject
+//
+//  Created by admin on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIStateObjectAndObservedObject/SwiftUIStateObjectAndObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateObjectAndObservedObjectApp.swift
+//  SwiftUIStateObjectAndObservedObject
+//
+//  Created by admin on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateObjectAndObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
